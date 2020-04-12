7ed29685a7f0ee1ddea0d6f604c3a4aef1d8744a
Jairajshetty <jayuchintu@gmail.com>
2020-04-12 20:06:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+/* eslint-disable */ 
+
+import React, { Component } from 'react';
+import Home from './containers/Home';
+
+class App extends Component {
+
+  render(){
+
+ 
+    return (
+      <Home/>
+      
+    );
+  }
+  
+};
+
+
+export default App;
